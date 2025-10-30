f36b9616638abff670d6364ce4b9a479cbc0a6ae
Adam El Ouahabi <105393528+ENERGETICKk@users.noreply.github.com>
2025-10-30 17:09:54 +0100

    .

---
+++ BBDD-Api/supabaseClient.js
@@ -0,0 +1,18 @@
+// supabase.js
+import { createClient } from '@supabase/supabase-js';
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from './.env';
+import dotenv from "dotenv";
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  console.error("❌ Faltan variables de entorno SUPABASE_URL o SUPABASE_ANON_KEY");
+  process.exit(1);
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+// Para ejecutar es con node <nombreArchivo>.js
